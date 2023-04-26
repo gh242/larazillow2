32021b3f8426e832dbba67b902e507508e91ec7f
charles <lin.th247@gmail.com>
2023-04-26 16:10:32 +0800

    53. Deleting Data

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::get('/hello', [IndexController::class, 'show']);
 
 // Route::resource('listing', ListingController::class)
 //   ->only(['index','show', 'create', 'store']);
-Route::resource('listing', ListingController::class)
-  ->except(['destroy']);
+// Route::resource('listing', ListingController::class)
+//   ->except(['destroy']);
+Route::resource('listing', ListingController::class);
