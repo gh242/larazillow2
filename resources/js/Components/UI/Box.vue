edb9ff2f73251e0994c8258b78bd3dd1534598d6
charles <lin.th247@gmail.com>
2023-04-27 23:48:42 +0800

    Revert "播放"
    This reverts commit 17374f838f9099ee8a734c874e62bc14e03bac32.

--- resources/js/Components/UI/Box.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="border border-gray-200 dark:border-gray-800 rounded-md p-4 shadow-sm dark:text-gray-300">
-    <slot/>
-  </div>
-</template>
--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -5,7 +5,7 @@
 
   <header class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full">
     <div class="container mx-auto">
-      <nav class="p-4 flex items-center justify-around">
+      <nav class="p-4 flex items-center justify-between">
         <div class="text-lg font-medium">
           <Link :href="route('listing.index')">Listings</Link>
         </div>
@@ -19,7 +19,7 @@
     </div>
   </header>
   <main class="container mx-auto p-4">
-    <div v-if="flashSuccess" class="mb-4 border rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
+    <div v-if="flashSuccess" class="mb-4 rounded-md shadow-sm border-green-200 dark:border-green-800 bg-green-50 dark:bg-green-900 p-2">
       {{ flashSuccess }}
     </div>
     <!-- <div>{{ y }}</div> -->
